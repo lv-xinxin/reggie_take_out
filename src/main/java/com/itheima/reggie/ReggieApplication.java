63a40c913b750670e781b9969045bfe61bb34ee9
zhosq <435221087@qq.com>
2022-04-25 11:31:25 +0800

    添加注解

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
 @SpringBootApplication
 @ServletComponentScan
+@EnableTransactionManagement
 @EnableCaching //开启spring cache缓存
 public class ReggieApplication {
     public static void main(String[] args) {
